e375e98a3ae4cd9135690cb012772770c895b913
Murdock Palmer Aubry <murdock@mail.cs.toronto.edu>
2024-09-07 16:41:37 -0400

    Updated bash scripts

--- shell/run_test_kaist512_vqf8.sh
+++ shell/run_test_kaist512_vqf8.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python scripts/rgb2ir_vqf8.py --steps 200 \
---indir /path/to/image \
---outdir /path/to/result \
---config /path/to/config \
+--indir dataset/KAIST512/visible/set07_V001_I00039.png \
+--outdir dataset/KAIST512/visible/out1.png \
+--config configs/latent-diffusion/kaist512-vqf8.yaml \
 --checkpoint /path/to/checkpoint \
 --ddim_eta 0.0
